c27ad6eeb9aca921748f96f8ccef98edefa91ed4
spritchard54 <>
2025-02-20 08:11:56 -0500

    maps updates

--- leaflet.js
+++ leaflet.js
@@ -2,8 +2,18 @@ var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
     denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
     aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
     golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
+    philadelphia = L.marker([39.95, -75.16]).bindPopup('This is Philadelphia, PA.');
 
-var cities = L.layerGroup([littleton, denver, aurora, golden]);
+var cities = L.layerGroup([littleton, denver, aurora, golden, philadelphia]);
+
+var yosemite = L.marker([37.86, -119.53]).bindPopup('<a href="trips.html">Yosemite National Park</a>'),
+    rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
+
+var parks = L.layerGroup([yosemite, rubyHill]);
+var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+});
 
 
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -16,8 +26,8 @@ var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
     attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
 
 var map = L.map('map', {
-    center: [39.73, -104.99],
-    zoom: 10,
+    center: [40.01224336270498, -97.76226241579424],
+    zoom: 5,
     layers: [osm, cities]
 });
 
@@ -27,19 +37,14 @@ var baseMaps = {
 };
 
 var overlayMaps = {
-    "Cities": cities
+    "Cities": cities,
 };
 
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
 
-var crownHill = L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.'),
-    rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
-    
-var parks = L.layerGroup([crownHill, rubyHill]);
-var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
-});
+
 
 layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
 layerControl.addOverlay(parks, "Parks");
+
+map.addLayer(parks);
