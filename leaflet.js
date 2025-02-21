6d8387682b938de50387c4b7e193acb3ce323d4a
spritchard54 <>
2025-02-21 18:04:03 -0500

    parks added to map on trips home page. All markers appear by default after adding .addLayer

--- leaflet.js
+++ leaflet.js
@@ -1,15 +1,33 @@
-var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
-    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
-    aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
-    golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
-    philadelphia = L.marker([39.95, -75.16]).bindPopup('This is Philadelphia, PA.');
+var npsIcon = L.icon({
+    iconUrl: 'images/nps.png',
+    iconSize:     [50,45], // size of the icon
+})
 
-var cities = L.layerGroup([littleton, denver, aurora, golden, philadelphia]);
 
-var yosemite = L.marker([37.86, -119.53]).bindPopup('<a href="trips.html">Yosemite National Park</a>'),
-    rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
+var philadelphia = L.marker([39.95, -75.16]).bindPopup('This is Philadelphia, PA.');
 
-var parks = L.layerGroup([yosemite, rubyHill]);
+var cities = L.layerGroup([philadelphia]);
+
+var yose = L.marker([37.86, -119.53],{icon: npsIcon}).bindPopup('<a href="trips.html">Yosemite National Park</a>'),
+    olym = L.marker([47.79, -123.61],{icon: npsIcon}).bindPopup('Olympic National Park.');
+    shen = L.marker([38.47, -78.45],{icon: npsIcon}).bindPopup('Shenandoah National Park.');
+    neri = L.marker([37.87, -81.05],{icon: npsIcon}).bindPopup('New River Gorge National Park.');
+    indu = L.marker([41.65, -87.05],{icon: npsIcon}).bindPopup('Indiana Dunes National Park.');
+    cuva = L.marker([41.28, -81.56],{icon: npsIcon}).bindPopup('Cuyahoga Valley National Park.');
+    zion = L.marker([37.29, -113.02],{icon: npsIcon}).bindPopup('Zion National Park.');
+    yell = L.marker([44.59, -110.56],{icon: npsIcon}).bindPopup('Yellowstone National Park.');
+    grte = L.marker([43.77, -110.73],{icon: npsIcon}).bindPopup('Grand Teton National Park.');
+    glac = L.marker([48.75, -113.78],{icon: npsIcon}).bindPopup('Glacier National Park.');
+    badl = L.marker([43.85, -102.33],{icon: npsIcon}).bindPopup('Badlands National Park.');
+    mora = L.marker([46.86, -121.69],{icon: npsIcon}).bindPopup('Mount Ranier National Park.');
+    maca = L.marker([37.18, -86.10],{icon: npsIcon}).bindPopup('Mammoth Cave National Park.');
+    crla = L.marker([42.92, -122.11],{icon: npsIcon}).bindPopup('Crater Lake National Park.');
+    redw = L.marker([41.39, -124.04],{icon: npsIcon}).bindPopup('Redwood National Park.');
+    jotr = L.marker([33.86, -115.91],{icon: npsIcon}).bindPopup('Joshua Tree National Park.');
+    grca = L.marker([36.26, -112.35],{icon: npsIcon}).bindPopup('Grand Canyon National Park.');
+    
+
+var parks = L.layerGroup([yose, olym, shen, neri, indu, cuva, zion, yell, grte, glac, badl, mora, maca, crla, redw, jotr, grca]);
 var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
@@ -27,7 +45,7 @@ var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
 
 var map = L.map('map', {
     center: [40.01224336270498, -97.76226241579424],
-    zoom: 5,
+    zoom: 4,
     layers: [osm, cities]
 });
 
